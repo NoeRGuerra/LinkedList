1f92686f80b3ffd9edc06221d76f6068c8021185
Noe <8528445+NoeRGuerra@users.noreply.github.com>
2024-10-20 15:06:55 -0400

    Add `pop()` `find()` and `toString()` functions
    - Replace placeholder `pop()` with working function
    - Add functionality to find a value in the list with `find()`
    - Display the linked list as a readable string with `toString()`

--- LinkedList.js
+++ LinkedList.js
@@ -58,20 +58,40 @@ export class LinkedList {
   }
 
   pop() {
-    // Remove last element (find element size-1 with at())
     let oldTail = this.tail;
-    let newTail = null; // Define new last element
-    // this.tail = newTail
+    let newTail = this.at(this.size() - 2); // Define penultimate node as new tail
+    newTail.nextNode = null;
+    this.tail = newTail;
     return oldTail;
   }
 
   find(value) {
     // Iterate through list until value is found and return index or null if not found
-    return;
+    let currentNode = this.head;
+    let counter = -1;
+    while (currentNode != null) {
+      counter += 1;
+      if (value === currentNode.value) {
+        break;
+      }
+      currentNode = currentNode.nextNode;
+    }
+    if (counter < 0) {
+      return null;
+    }
+    return counter;
   }
 
   toString() {
     // Return string in format ( value ) -> ( value ) -> ( value ) -> null
-    return;
+    let stringRepr = "";
+    let currentNode = this.head;
+    while (currentNode != null) {
+      let valueStr = `( ${currentNode.value} ) -> `;
+      stringRepr = stringRepr + valueStr;
+      currentNode = currentNode.nextNode;
+    }
+    stringRepr = stringRepr + "null";
+    return stringRepr;
   }
 }
